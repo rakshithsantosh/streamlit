7524f07a7229f93324de6bcde019657aa08cfd6f
rakshith_s <rakshithsantosh@gmail.com>
2025-05-21 16:10:27 +0530

    started with streamlit

---
+++ chatApp/main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Chat App")
+st.subheader("Brewed with streamlit")
+st.text('Welcome to your iteractive app')
+st.write("choose your fav animie ")
+
+chai = st.selectbox("your fav animie",["dragonballz","overlord","black clover"])
+
+st.write(f"you choose{chai} nice choice")
+
+st.success("you fav animie is cool")
